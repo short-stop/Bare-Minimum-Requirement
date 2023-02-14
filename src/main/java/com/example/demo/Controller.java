7faad3ce6a4bdc79adf35d143858b2db0001edd8
short-stop <pruning1126@gmail.com>
2023-02-14 16:59:50 +0900

    first

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping("/")
-    public String TodoPrinting() {
+    public String TodoPrinting1() {
         return "To-do Application";
     }
 }
